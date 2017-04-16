07ab6cebba4cb089ad4637cbebb83ca3069f9897
fullautomedia <taryn@fullautomedia.com>
2017-04-16 17:48:01 -0600

    included tutorial

--- src/pages/pages.ts
+++ src/pages/pages.ts
@@ -7,8 +7,8 @@ import { DashboardPage } from './dashboard/dashboard';
 // The page the user lands on after opening the app and without a session
 
 //TODO: for devl, skip tutorial
-// export const FirstRunPage = TutorialPage;
-export const FirstRunPage = DashboardPage;
+export const FirstRunPage = TutorialPage;
+// export const FirstRunPage = DashboardPage;
 
 // The main page the user will see as they use the app over a long period of time.
 // Change this if not using tabs
